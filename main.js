e90c815d421d6af5856d2179d8278c172cf54f76
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-02-02 17:46:23 +0000

    Adding elements

--- main.js
+++ main.js
@@ -5,14 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imgList = [
+    'pic1.jpg',
+    'pic2.jpg',
+    'pic3.jpg',
+    'pic4.jpg',
+    'pic5.jpg'
+]
 /* Declaring the alternative text for each image file */
-
+const altObj = {
+    'pic1.jpg': 'Close up of a human eye',
+    'pic2.jpg': 'Rock formation',
+    'pic3.jpg': 'Purple and White Flowers',
+    'pic4.jpg': 'Heiroglyphics',
+    'pic5.jpg': 'Monarch Butterfly'
+}
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for (let i = 0; i < imgList.length; i++) {
+    const element = imgList[i];
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imgList);
+    newImage.setAttribute('alt', altObj);
+    thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
